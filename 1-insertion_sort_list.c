8af2f7c011b1c74f9b4b63b7cf0171088242b5ff
Ochieng24 <edwine.ochi24@gmail.com>
2023-10-22 07:50:53 -0700

    fixes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,52 +1,53 @@
 #include "sort.h"
-/**
-  * insertion_sort_list - sorts a doubly linked list of int in ascending order
-  * @list: head of the list
-  * Return: void
-  */
 
+/**
+ * insertion_sort_list - sorts a doubly linked list in asce order
+ * @list: Double pointer to the head of the list
+ * Return: void
+ */
 void insertion_sort_list(listint_t **list)
 {
-	/*used to traverse the list during sorting*/
-	listint_t *temp;
+if (*list == NULL || (*list)->next == NULL)
+return; /* If the list is empty or has only one element, it's already sorted.*/
 
-	/* checks if the list is empty or has only one element */
-	if (*list == NULL || (*list)->next == NULL)
-		return;
+listint_t *current = (*list)->next; /*Initialize the 'current' pointer*/
 
-	/* assign temp to second node. the first is already sorted */
-	temp = (*list)->next;
-	while (temp)
-	{
-		while ((temp->prev) && (temp->prev->n > temp->n))
-		{
-			temp = swap_node(temp, list);
-			print_list(*list);
-		}
-		temp = temp->next;
-	}
+while (current != NULL)
+{
+listint_t *temp = current; /*'temp' points tcurren node*/
+
+while (temp->prev != NULL && temp->n < temp->prev->n)
+{
+swap_nodes(temp, list); /*Swap the nodes*/
+print_list(*list); /* Print the list after swapping.*/
+}
+
+current = current->next; /*Move to the next node to continue sorting.*/
+}
 }
 
 /**
-  * swap_node - swaps the value in the list
-  * @temp: point to second node
-  * @list: point to head of the list
-  * Return: current node
-  */
-listint_t *swap_node(listint_t *temp, listint_t **list)
+ * swap_nodes - swaps two nodes in a doubly linked list
+ * @node: The node to be swapped
+ * @list: Double pointer to the head of the list
+ */
+void swap_nodes(listint_t *node, listint_t **list)
 {
-	/* behind is node before current, current is node to be swapped*/
-	listint_t *current = temp, *behind = temp->prev;
+listint_t *prev_node = node->prev; /*'prev_node' points to prev node.*/
 
-	behind->next = current->next;
-	if (current->next)
-		current->next->prev = behind;
-	current->next = behind;
-	current->prev = behind->prev;
-	behind->prev = current;
-	if (current->prev)
-		current->prev->next = current;
-	else
-		*list = current;
-	return (current);
+if (prev_node)
+prev_node->next = node->next; /*Adjust the 'next' pointer*/
+
+if (node->next)
+node->next->prev = prev_node; /*Adjust the 'prev' pointer*/
+
+node->prev = prev_node->prev; /* Update the next pointer*/
+node->next = prev_node; /* Set the 'next' pointer*/
+
+if (prev_node->prev)
+prev_node->prev->next = node; /*Update the 'next' ptr*/
+else
+*list = node; /*Update the head of the list if needed.*/
+
+prev_node->prev = node; /*Update the 'prev' ptr */
 }
