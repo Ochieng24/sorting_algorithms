3d30b594c0f2e4a05fab769c175d992c93813b38
Ochieng24 <edwine.ochi24@gmail.com>
2023-10-22 08:46:53 -0700

    added comments

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,41 +1,41 @@
 #include "sort.h"
 /**
- *_swap - swaps two values of type int
- *@a: value 1
- *@b: value 2
+ * _swap - Swaps two integer values.
+ * @a: First value to swap.
+ * @b: Second value to swap.
  */
 void _swap(int *a, int *b)
 {
-	int temp;
+int temp;  /* Temporary variable */
 
-	temp = *a;
-	*a = *b;
-	*b = temp;
+temp = *a;  /* Store the value of 'a' in 'temp' */
+*a = *b;    /* Assign 'b' to 'a' */
+*b = temp;  /* Assign 'temp' to 'b' */
 }
 
 /**
- *selection_sort - algo that sorts an array of ints in ascending order
- *@array: array to sort
- *@size: length of array to sort
+ * selection_sort - Sorts an array of integer.
+ * @array: Array to sort.
+ * @size: Length of the array to sort.
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t j, i, min;
+size_t j, i, min;/* Loop variables and minimum index */
 
-	for (i = 0; i < size - 1; i++)
-	{
-		min = i;
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[min])
-			{
-				min = j;
-			}
-		}
-		if (min != i)
-		{
-			_swap(&array[i], &array[min]);
-			print_array(array, size);
-		}
-	}
+for (i = 0; i < size - 1; i++)
+{
+min = i;/* Assume the current index is the minimum */
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min])
+{
+min = j;/* Update minimum index */
+}
+}
+if (min != i)
+{
+_swap(&array[i], &array[min]);/* Swap elements if necessary */
+print_array(array, size);/* Print the array after each swap */
+}
+}
 }
