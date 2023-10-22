683a8f588f7610802bdddaa3c6b3c44055fb1dce
Ochieng24 <edwine.ochi24@gmail.com>
2023-10-22 09:00:40 -0700

    fixes

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,80 +1,84 @@
 #include "sort.h"
+
 /**
-  * _swap - swap two values
-  * @a: first value
-  * @b: second value
-  * Return: void
-  */
+ * _swap - Swaps two values.
+ * @a: First value.
+ * @b: Second value.
+ * Return: void.
+ */
 void _swap(int *a, int *b)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+int temp = *a;/* Store the value of 'a' in a temporary variable */
+*a = *b;/* Assign the value of 'b' to 'a' */
+*b = temp;/* Assign the temporary value to 'b' */
 }
 
 /**
-  * lomuto - partition
-  * @low: the lowest index
-  * @high: the highest index
-  * @array: pointer to array of list to partion
-  * @size: size of array
-  * Return: pivot index
-  */
+ * lomuto - Lomuto partition.
+ * @array: Pointer to the array to partition.
+ * @size: Size of the array.
+ * @low: The lowest index.
+ * @high: The highest index.
+ * Return: Pivot index.
+ */
 int lomuto(int *array, size_t size, int low, int high)
 {
-	/* initialize pivot*/
-	int i, j, pivot = array[high];
+int i, j, pivot = array[high];  /* Initialize pivot */
 
-	i = low - 1;
+i = low - 1;
 
-	for (j = low; j <= high - 1; j++)
-	{
-		if (array[j] < pivot)
-		{
-			i++;
-			if (array[i] != array[j])
-			{
-				_swap(&array[i], &array[j]);
-				print_array(array, size);
-			}
-		}
-	}
-	if (array[i + 1] != array[high])
-	{
-		_swap(&array[i + 1], &array[high]);
-		print_array(array, size);
-	}
-	return (i + 1);
+for (j = low; j <= high - 1; j++)
+{
+if (array[j] < pivot)
+{
+i++;  /* Increment 'i' when a smaller element is found */
+if (array[i] != array[j])
+{
+_swap(&array[i], &array[j]);  /* Swap elements */
+print_array(array, size);  /* Print the array after each swap */
+}
+}
+}
+
+if (array[i + 1] != array[high])
+{
+_swap(&array[i + 1], &array[high]);  /* Swap elements */
+print_array(array, size);  /* Print the array after the final swap */
+}
+
+return (i + 1);  /* Return the pivot index */
 }
 
 /**
-  * _quick_sort - sort portion of array and divide into partion and sort
-  * @array: pointer to array of list to partion
-  * @low: the lowest index
-  * @high: the highest index
-  * @size: size of array
-  * Return: void
-  */
+ * _quick_sort - Sort a portion of the array.
+ * @array: Pointer to the array to be sorted.
+ * @size: Size of the array.
+ * @low: The lowest index.
+ * @high: The highest index.
+ * Return: void.
+ */
 void _quick_sort(int *array, size_t size, int low, int high)
 {
-	if (low < high)
-	{
-		int mid = lomuto(array, size, low, high);
+if (low < high)
+{
+int mid = lomuto(array, size, low, high);/* Find the pivot */
 
-		_quick_sort(array, size, low, mid - 1);
-		_quick_sort(array, size, mid + 1, high);
-	}
+_quick_sort(array, size, low, mid - 1);/* Sort left partition */
+_quick_sort(array, size, mid + 1, high);/* Sort right partition */
+}
 }
 
 /**
-  * quick_sort - implement the sort
-  * @array: pointer to array of list to portion
-  * @size: size of the array
-  * Return: void
-  */
+ * quick_sort - Implement the quick sort algorithm.
+ * @array: Pointer to the array to be sorted.
+ * @size: Size of the array.
+ * Return: void.
+ */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
-	_quick_sort(array, size, 0, size - 1);
+if (array == NULL || size < 2)
+return;/* Return if the array is empty */
+
+_quick_sort(array, size, 0, size - 1);/* Call sorting funct */
 }
+
